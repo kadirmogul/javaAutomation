7c0fd8adf9a9e5fac8d5843bc87e1584720e769e
kadirmogul <158027198+kadirmogul@users.noreply.github.com>
2024-12-02 15:49:22 +0300

    Update scrollControl.java

--- src/main/java/scrollControl.java
+++ src/main/java/scrollControl.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class scrollControl {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver =new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //Sayfa scroll etmek
+        String baseURl8="https://demo.guru99.com/test/guru99home";
+        driver.get(baseURl8);
+        JavascriptExecutor js =(JavascriptExecutor) driver;
+        //js.executeScript("window.scrollBy(0,500)");
+        WebElement linux = driver.findElement(By.linkText("Linux"));
+        //js.executeScript("arguments[0].scrollIntoView();",linux);
+       js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+
+        driver.quit();
+
+    }
+
 }
