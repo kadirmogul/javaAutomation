d6774efd1d235e7c63a427ca53b3b1f3ce6d67b4
kadirmogul <158027198+kadirmogul@users.noreply.github.com>
2024-12-02 15:46:46 +0300

    Update mouseHover.java
    v2

--- src/main/java/org/example/mouseHover.java
+++ src/main/java/org/example/mouseHover.java
@@ -4,16 +4,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 
 
-public class Main {
+public class mouseHover {
     public static void main(String[] args) {
 
         WebDriverManager.chromedriver().setup();
